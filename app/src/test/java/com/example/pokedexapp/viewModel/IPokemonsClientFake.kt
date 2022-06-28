5c22b088232c8587db593e04f16ffe81e0c908fa
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-06-28 13:57:29 -0300

    Aplicação testes na viewModel

--- app/src/test/java/com/example/pokedexapp/viewModel/IPokemonsClientFake.kt
+++ app/src/test/java/com/example/pokedexapp/viewModel/IPokemonsClientFake.kt
@@ -1,4 +1,28 @@
 package com.example.pokedexapp.viewModel
 
-class IPokemonsClientFake {
+import com.example.pokedexapp.data.Pokemon
+import com.example.pokedexapp.repository.IPokemonsRepository
+
+class IPokemonsClientFake(val string: String) : IPokemonsRepository {
+    override suspend fun getPokedex(): List<Pokemon> {
+        return if (string == "OK-200") {
+            mockPokemon
+        } else if (string == "ERRO-Generic") {
+            throw Throwable(string)
+        } else listOf()
+    }
 }
+
+val mockPokemon = listOf(
+    Pokemon(
+        "69",
+        "Bulbasaur Candy",
+        25,
+        "2 km",
+        "0.71 m",
+        1,
+        "http://www.serebii.net/pokemongo/pokemon/001.png",
+        listOf(1.58), "Bulbasaur", listOf(), "001", 0.69, "20:00", listOf("Grass", "Poison"),
+        listOf("Fire", "Ice", "Flying", "Psychic"), "6.9 kg", true
+    )
+)
--- app/src/test/java/com/example/pokedexapp/viewModel/MainViewModelTest.kt
+++ app/src/test/java/com/example/pokedexapp/viewModel/MainViewModelTest.kt
@@ -1,5 +1,118 @@
 package com.example.pokedexapp.viewModel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.pokedexapp.data.PokemonApiResult
+import com.example.pokedexapp.test_utils.MainCoroutineRule
+import com.example.pokedexapp.test_utils.getOrAwaitValue
+import io.mockk.InternalPlatformDsl.toArray
 import junit.framework.TestCase
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class MainViewModelTest : TestCase()
+@RunWith(JUnit4::class)
+class MainViewModelTest : TestCase() {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    lateinit var mainViewModelOk: MainViewModel
+    lateinit var mainViewModelError: MainViewModel
+
+    @Before
+    fun setup() {
+        mainViewModelOk = MainViewModel(
+            // PokemonsRepository(
+            IPokemonsClientFake("OK-200")
+            // )
+        )
+        mainViewModelError = MainViewModel(
+            // PokemonsRepository(
+            IPokemonsClientFake("ERRO-Generic")
+            // )
+        )
+    }
+
+    @Test
+    fun `Set ID of _coinSelected and return the same ID in coinSelected`() {
+        // Given
+        val id = 1
+
+        // Then
+        mainViewModelOk.setPokemon(id)
+        val result = mainViewModelOk.pokemonSelected.getOrAwaitValue().id
+
+        // When
+        Assert.assertEquals(id, result)
+    }
+
+    @Test
+    fun `When the list of coins isn't empty, not require other api call`() {
+        // Given
+        val list = mainViewModelOk.pokemonItem
+        var result = true
+        var result2 = true
+
+        // Then
+        mainViewModelOk.getCoinsFromRetrofit()
+        when (list) {
+            is PokemonApiResult.Success<*> -> {
+                result = list.getOrAwaitValue().toArray().isEmpty() // Return False
+            }
+        }
+
+        mainViewModelOk.getCoinsFromRetrofit()
+
+        when (mainViewModelOk.pokemonItem) {
+            is PokemonApiResult.Success<*> -> {
+                result2 = list.getOrAwaitValue().toArray().isEmpty() // Return False
+            }
+        }
+
+        // When
+        Assert.assertEquals(result, result2)
+    }
+
+    @Test
+    fun `When setFavorite set true`() {
+        // Given
+        val bulbasaur = mockPokemon[0]
+        bulbasaur.isFavorite = true // Is favorite
+
+        // When
+        mainViewModelOk.getCoinsFromRetrofit()
+        mainViewModelOk.setPokemon(1)
+        mainViewModelOk.setFavorite(true)
+
+        // Then
+
+        Assert.assertEquals(
+            bulbasaur.isFavorite,
+            mainViewModelOk.pokemonSelected.getOrAwaitValue().isFavorite
+        )
+    }
+
+    @Test
+    fun `When setFavorite set false`() {
+        // Given
+        val bulbasaur = mockPokemon[0]
+        bulbasaur.isFavorite = false // Is favorite
+
+        // When
+        mainViewModelOk.getCoinsFromRetrofit()
+        mainViewModelOk.setPokemon(1)
+        mainViewModelOk.setFavorite(false)
+
+        // Then
+
+        Assert.assertEquals(
+            bulbasaur.isFavorite,
+            mainViewModelOk.pokemonSelected.getOrAwaitValue().isFavorite
+        )
+    }
+}
