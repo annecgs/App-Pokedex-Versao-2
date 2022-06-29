1b14475aeb8a56c243b31ed4300441d674886bd8
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-06-29 15:52:40 -0300

    Aplicacção de testes na UI

--- app/src/androidTest/java/com/example/pokedexapp/ui/home/NavigationTest.kt
+++ app/src/androidTest/java/com/example/pokedexapp/ui/home/NavigationTest.kt
@@ -1,5 +1,46 @@
 package com.example.pokedexapp.ui.home
 
-class NavationTest {
-    
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.pokedexapp.MainActivity
+import com.example.pokedexapp.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+    @Test
+    fun navigationHome() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        // Tela inicial com todos os pokemons
+        onView(withId(R.id.navigation_home))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.tv_mainTitle))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.tv_mainDate))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.searchView))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navagationFavoritos() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        // Tela de favoritos
+        onView(withId(R.id.navigation_notifications))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.tv_mainTitle))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.tv_mainDate))
+            .check(matches(isDisplayed()))
+    }
 }
