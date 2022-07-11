884084bec7e780c764a7361abbb05ee01fe1186b
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-07-11 10:46:54 -0300

    Revert "Melhorias na trativa de erros"
    This reverts commit 33ae3de758b367e03606a929daeecafe2638041e.

--- frontend/src/androidTest/java/com/example/frontend/ExampleInstrumentedTest.kt
+++ frontend/src/androidTest/java/com/example/frontend/ExampleInstrumentedTest.kt
@@ -1,10 +1,12 @@
 package com.example.frontend
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.* // ktlint-disable no-wildcard-imports
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
